579aef8db2aa8900bde45491ff722fa97ec96b4b
Snaylo <42110788+ScottJamesNaylor@users.noreply.github.com>
2021-06-05 19:29:42 +0200

    Unreal Project

--- communication.py
+++ communication.py
@@ -1,38 +1,16 @@
-import socket
-import struct
-import traceback
-import logging
-import time
+
 import numpy as np
+import socket
+
+host, port = "127.0.0.1", 25001
+data = "true"
 
 
-def sending_and_receiving():
-    s = socket.socket()
-    socket.setdefaulttimeout(None)
-    print('socket created ')
-    port = 60000
-    s.bind(('127.0.0.1', port))  # local host
-    s.listen(30)  # listening for connection for 30 sec?
-    print('socket listening ... ')
-    while True:
-        try:
-            c, addr = s.accept()  # when port connected
-            bytes_received = c.recv(4000)  # received bytes
-            array_received = np.frombuffer(bytes_received, dtype=np.float32)  # converting into float array
 
-            # nn_output = return_prediction(array_received) #NN prediction (e.g. model.predict())
-            #bytes_to_send = struct.pack('%sf' % len(array), *array)  # converting float to byte
-            #c.sendall(array)  # sending back
-
-            c.sendall(bytearray([0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1]))
-            c.close()
-            print(array_received)
-        except Exception as e:
-            logging.error(traceback.format_exc())
-            print("error")
-            c.sendall(bytearray([]))
-            c.close()
-            break
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-sending_and_receiving()
+s.connect((host, port))
+data = "happy"
+while True:
+    s.sendall(data.encode("utf-8"))
