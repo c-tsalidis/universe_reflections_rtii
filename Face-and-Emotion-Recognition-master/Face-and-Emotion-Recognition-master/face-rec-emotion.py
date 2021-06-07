e67a2a52ac56e1fff93bd9b979bb353c3622ab97
Snaylo <scottjamesnaylor@gmail.com>
2021-06-07 14:56:28 +0200

    Mixed human recognition and heart rate monitor from same webcam capture
    Co-Authored-By: Christian Tsalidis <7976352+c-tsalidis@users.noreply.github.com>
    Co-Authored-By: balazsivanyi <28901674+balazsivanyi@users.noreply.github.com>

--- Face-and-Emotion-Recognition-master/Face-and-Emotion-Recognition-master/face-rec-emotion.py
+++ Face-and-Emotion-Recognition-master/Face-and-Emotion-Recognition-master/face-rec-emotion.py
@@ -19,6 +19,70 @@ USE_WEBCAM = True  # If false, loads video file source
 from pythonosc.dispatcher import Dispatcher
 from typing import List, Any
 
+
+# HEARTRATE MONITOR VARIABLES AND METHODS
+
+# Helper Methods
+def buildGauss(frame, levels):
+    pyramid = [frame]
+    for level in range(levels):
+        frame = cv2.pyrDown(frame)
+        pyramid.append(frame)
+    return pyramid
+
+def reconstructFrame(pyramid, index, levels):
+    filteredFrame = pyramid[index]
+    for level in range(levels):
+        filteredFrame = cv2.pyrUp(filteredFrame)
+    filteredFrame = filteredFrame[:videoHeight, :videoWidth]
+    return filteredFrame
+
+realWidth = 320
+realHeight = 240
+videoWidth = 160
+videoHeight = 120
+videoChannels = 3
+videoFrameRate = 15
+
+
+# Color Magnification Parameters
+levels = 3
+alpha = 170
+minFrequency = 1.0
+maxFrequency = 2.0
+bufferSize = 150
+bufferIndex = 0
+
+# Output Display Parameters
+font = cv2.FONT_HERSHEY_SIMPLEX
+loadingTextLocation = (20, 30)
+bpmTextLocation = (videoWidth//2 + 5, 30)
+fontScale = 1
+fontColor = (255,255,255)
+lineType = 2
+boxColor = (0, 255, 0)
+boxWeight = 3
+
+# Initialize Gaussian Pyramid
+firstFrame = np.zeros((videoHeight, videoWidth, videoChannels))
+firstGauss = buildGauss(firstFrame, levels+1)[levels]
+videoGauss = np.zeros((bufferSize, firstGauss.shape[0], firstGauss.shape[1], videoChannels))
+fourierTransformAvg = np.zeros((bufferSize))
+
+# Bandpass Filter for Specified Frequencies
+frequencies = (1.0*videoFrameRate) * np.arange(bufferSize) / (1.0*bufferSize)
+mask = (frequencies >= minFrequency) & (frequencies <= maxFrequency)
+
+# Heart Rate Calculation Variables
+bpmCalculationFrequency = 15
+bpmBufferIndex = 0
+bpmBufferSize = 10
+bpmBuffer = np.zeros((bpmBufferSize))
+
+
+
+
+
 dispatcher = Dispatcher()
 count_before_average = 50
 emotional_block = 0
@@ -47,9 +111,13 @@ from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.udp_client import SimpleUDPClient
 
 ip = "127.0.0.1"
-port = 8108
 
-client = SimpleUDPClient(ip, port)  # Create client
+
+emotions_osc_port = 8108
+heartrate_osc_port = 8000
+
+emotions_client = SimpleUDPClient(ip, emotions_osc_port)  # Create emotions client
+heartrate_client = SimpleUDPClient(ip, heartrate_osc_port)  # Create heartrate client
 
 # parameters for loading data and images
 emotion_model_path = './models/emotion_model.hdf5'
@@ -109,9 +177,9 @@ process_this_frame = True
 def take_average_emotion(total):
     average = total / count_before_average
     if average >= 0.5:
-        client.send_message("/happy", 1)
+        emotions_client.send_message("/happy", 1)
     else:
-        client.send_message("/sad", 1)
+        emotions_client.send_message("/sad", 1)
     print(average)
     global emotional_block
     emotional_block = 0
@@ -174,10 +242,64 @@ if (USE_WEBCAM == True):
 else:
     cap = cv2.VideoCapture('./test/testvdo.mp4')  # Video file source
 
+
+i = 0
 while cap.isOpened():  # True:
     ret, frame = cap.read()
 
     # frame = video_capture.read()[1]
+
+
+
+
+
+    # HEART RATE MONITORING
+    detectionFrame = frame[int(videoHeight / 2):int(realHeight - videoHeight / 2), int(videoWidth / 2):int(realWidth - videoWidth / 2), :]
+    # Construct Gaussian Pyramid
+    videoGauss[bufferIndex] = buildGauss(detectionFrame, levels + 1)[levels]
+    fourierTransform = np.fft.fft(videoGauss, axis=0)
+
+    # Bandpass Filter
+    fourierTransform[mask == False] = 0
+
+    # Grab a Pulse
+    if bufferIndex % bpmCalculationFrequency == 0:
+        i = i + 1
+        for buf in range(bufferSize):
+            fourierTransformAvg[buf] = np.real(fourierTransform[buf]).mean()
+        hz = frequencies[np.argmax(fourierTransformAvg)]
+        bpm = 60.0 * hz
+        bpmBuffer[bpmBufferIndex] = bpm
+        bpmBufferIndex = (bpmBufferIndex + 1) % bpmBufferSize
+
+    # Amplify
+    filtered = np.real(np.fft.ifft(fourierTransform, axis=0))
+    filtered = filtered * alpha
+
+    # Reconstruct Resulting Frame
+    filteredFrame = reconstructFrame(filtered, bufferIndex, levels)
+    outputFrame = detectionFrame + filteredFrame
+    outputFrame = cv2.convertScaleAbs(outputFrame)
+
+    bufferIndex = (bufferIndex + 1) % bufferSize
+
+    frame[int(videoHeight / 2):int(realHeight - videoHeight / 2), int(videoWidth / 2):int(realWidth - videoWidth / 2),
+    :] = outputFrame
+    cv2.rectangle(frame, (int(videoWidth / 2), int(videoHeight / 2)),
+                  (int(realWidth - videoWidth / 2), int(realHeight - videoHeight / 2)), boxColor, boxWeight)
+    if i > bpmBufferSize:
+        cv2.putText(frame, "BPM: %d" % bpmBuffer.mean(), bpmTextLocation, font, fontScale, fontColor, lineType)
+        heartrate_client.send_message("/bpm", int(bpmBuffer.mean()))
+    else:
+        cv2.putText(frame, "Calculating BPM...", loadingTextLocation, font, fontScale, fontColor, lineType)
+
+
+
+
+
+
+
+
 
     # To print the facial landmarks
     # landmrk = face_recognition.face_landmarks(frame)
